aaf0d64d7dc712d32ddf3fbea2f9383b001eff0d
rippnitaa <145269418+Rifnita@users.noreply.github.com>
2025-05-28 12:12:39 +0700

    Delete UAS_122140031 directory

--- UAS_122140031/UAS_122140031/views/default.py
+++
@@ -1,6 +0,0 @@
-from pyramid.view import view_config
-
-
-@view_config(route_name='home', renderer='UAS_122140031:templates/mytemplate.jinja2')
-def my_view(request):
-    return {'project': 'UAS_122140031'}
